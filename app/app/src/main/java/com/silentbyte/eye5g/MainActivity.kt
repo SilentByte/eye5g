a470ade384d2b0089acf052d7df403a02bdeef55
Rico A. Beti <rico.beti@silentbyte.com>
2021-06-04 23:36:18 +0800

    Add eye icon for FAB

--- app/app/src/main/java/com/silentbyte/eye5g/MainActivity.kt
+++ app/app/src/main/java/com/silentbyte/eye5g/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
 import com.silentbyte.eye5g.databinding.ActivityMainBinding
 import com.vmadalin.easypermissions.EasyPermissions
 import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
@@ -42,10 +41,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-
+        binding.fab.setOnClickListener {
             if(!hasAllPermissions()) {
                 requestAllPermissions()
             } else {
